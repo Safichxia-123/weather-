761fd1216bb3fd2c9037de3daa1bda30a75b22a6
Safichxia-123 <150838629+Safichxia-123@users.noreply.github.com>
2023-11-14 09:24:46 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+function App() {
+
+    //const url = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={5167520dec0d223c7f387b05c63f86da }'
+
+
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
